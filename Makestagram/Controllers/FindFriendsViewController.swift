cf161f3edcd736a7b09cc3b32b108bbedb8d86bf
mewilson96 <maryeliza96@yahoo.com>
2017-06-29 16:57:38 -0400

    Setting Up Tabs
    created tabs for home, find friends, and plus

---
+++ Makestagram/Controllers/FindFriendsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FindFriendsViewController.swift
+//  Makestagram
+//
+//  Created by Mary Wilson on 6/29/17.
+//  Copyright © 2017 Mary Wilson. All rights reserved.
+//
+
+import UIKit
+
+class FindFriendsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+   
+}
---
+++ Makestagram/Controllers/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  Makestagram
+//
+//  Created by Mary Wilson on 6/29/17.
+//  Copyright © 2017 Mary Wilson. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
