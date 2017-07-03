d45d4072601cf4b653ad4016b050c33be8b96656
mewilson96 <maryeliza96@yahoo.com>
2017-07-03 13:41:35 -0400

    Like/ Dislike
    Implemented the code in the LikeService class to increment and decrement likes

--- Makestagram/Services/LikeService.swift
+++ Makestagram/Services/LikeService.swift
@@ -11,11 +11,11 @@ import FirebaseDatabase
 
 struct LikeService {
     
-    static func create (for post: Post, sucess: @escaping (Bool) -> Void) {
+    static func create (for post: Post, success: @escaping (Bool) -> Void) {
         
         // Each posts needs a key, so we're checking that a post has one and if not it returns false
         guard let key = post.key else {
-            return sucess(false)
+            return success(false)
         }
         
         //creating a reference to the current user's UIS
@@ -26,33 +26,65 @@ struct LikeService {
         likesRef.setValue(true) {(error, _) in
             if let error = error {
                 assertionFailure(error.localizedDescription)
-                return sucess (false)
+                return success (false)
             }
             
-            return sucess(true)
+            
+            let likeCountRef = Database.database().reference().child("posts").child(post.poster.uid).child(key).child("like_count")
+            likeCountRef.runTransactionBlock({ (mutableData) -> TransactionResult in
+                let currentCount = mutableData.value as? Int ?? 0
+                
+                mutableData.value = currentCount + 1
+                
+                return TransactionResult.success(withValue: mutableData)
+            }, andCompletionBlock: { (error, _, _) in
+                if let error = error {
+                    assertionFailure(error.localizedDescription)
+                    success(false)
+                    
+                }else {
+                    success(true)
+                    
+                }
+            })
         }
+        
         
     }
     
-    static func delete (for post: Post, sucess: @escaping (Bool) -> Void) {
+    static func delete (for post: Post, success: @escaping (Bool) -> Void) {
         // Each posts needs a key, so we're checking that a post has one and if not it returns false
         guard let key = post.key else {
-            return sucess(false)
+            return success(false)
         }
         
         //creating a reference to the current user's UID
         let currentUID = User.current.uid
         
-        //returns a Bool on whether the network request was sucessfully executed and the like data was saved to the database
+        //returns a Bool on whether the network request was sucessfully executed and the dislikelike data was saved to the database
         let likesRef = Database.database().reference().child("postLikes").child(key).child(currentUID)
         likesRef.setValue(nil) {(error, _) in
             if let error = error {
                 assertionFailure(error.localizedDescription)
-                return sucess (false)
+                return success (false)
             }
             
-            return sucess(true)
+            let likeCountRef = Database.database().reference().child("posts").child(post.poster.uid).child(key).child("like_count")
+            
+            likeCountRef.runTransactionBlock( { (mutableData) -> TransactionResult in
+                let currentCount = mutableData.value as? Int ?? 0
+                
+                mutableData.value = currentCount - 1
+                
+                return TransactionResult.success(withValue: mutableData)
+            }, andCompletionBlock: { (error, _, _) in
+                if let error = error {
+                    assertionFailure(error.localizedDescription)
+                    success(false)
+                } else {
+                    success(true)
+                }
+            })
         }
-
     }
 }
